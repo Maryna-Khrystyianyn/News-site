00bb19c660b9b28031b848a70c4f596174e6d3b9
Elham Khodabandeh <elham.khodabandeh@dci-student.org>
2025-09-02 13:11:55 +0200

    componenten erstellt, alles verknüpft, apis eingesetzt und alles gefetcht, nur noch stylen

--- src/components/footer/FollowSectionPhotos.jsx
+++ src/components/footer/FollowSectionPhotos.jsx
@@ -1,21 +1,27 @@
 import { useEffect, useState } from "react";
+import { createClient } from "pexels";
+
+const API_KEY_Photo = import.meta.env.VITE_PHOTO_API_KEY;
+const client = createClient(API_KEY_Photo);
+
+// All requests made with the client will be authenticated
 
 export const FollowSectionPhotos = () => {
-    const [photos, setPhoto] = useState([]);
-    
-      useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/photos?albumId=1")
-          .then((res) => res.json())
-          .then((data) => setPhoto(data.slice(0, 9))); // nur 5 Kommentare
-      }, []);
-    
-      return (
-        <div>
-          {photos.map((c) => (
-            <div key={c.id}>
-              <img src={c.thumbnailUrl} alt={c.title} />
-            </div>
-          ))}
+  const [photos, setPhoto] = useState([]);
+
+  useEffect(() => {
+    client.photos.curated({ per_page: 9 }).then((result) => {
+      setPhoto(result.photos); // result contains { page, per_page, photos }
+    });
+  }, []);
+
+  return (
+    <div>
+      {photos.map((c) => (
+        <div key={c.id}>
+          <img src={c.src.medium} alt={c.alt} />
         </div>
-      );
-}
+      ))}
+    </div>
+  );
+};
