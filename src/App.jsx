1b00300fc4c5f015e81049db4bb93f79c8110b33
Elham Khodabandeh <elham.khodabandeh@dci-student.org>
2025-09-09 08:52:40 +0200

    default geändert

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,26 @@ import AppRoutes from "./routes";
 import Navigator from "./components/header/Navigator";
 import Writer from "./components/writer/Writer";
 import { Footer } from "./components/footer/Footer";
-import { CategorySlider } from "./components/sliders/CategorySlider/CategorySlider"
+import { CategorySlider } from "./components/sliders/CategorySlider/CategorySlider";
 import { NewsSlider } from "./components/sliders/newsSlider/NewsSlider";
 // import { AboutUsPage } from "./components/aboutUs/AboutUsPage";
 
-function App() {
+export default function App() {
   return (
     <>
-  
-    <div className="w-[1512px] m-auto">
-      <Header />
-      <Navigator />
-       <CategorySlider/>
-       <NewsSlider/>
-      <div className="mt-130">
-        <Writer />
+      <div className="w-[1512px] m-auto">
+        <Header />
+        <Navigator />
+        <CategorySlider />
+        <NewsSlider />
+        <div className="mt-130">
+          <Writer />
+        </div>
+        <AppRoutes />
+        <div className="absolute left-0 mt-60">
+          <Footer />
+        </div>
       </div>
-      <AppRoutes />
-      <div className="absolute left-0 mt-60">
-        <Footer />
-      </div>
-    </div>
     </>
   );
 }
-
-export default App;
