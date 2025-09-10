59eb514f1d77bf59c386802f2459d976b9635c85
khatuna120 <shaveshovak@gmail.com>
2025-09-10 09:45:44 +0200

    css responsive

--- src/components/sliders/category-slider/CategorySlider.jsx
+++ src/components/sliders/category-slider/CategorySlider.jsx
@@ -13,7 +13,25 @@ export const CategorySlider = () => {
       slidesPerView={8}
       navigation={true}
       modules={[Navigation]}
-      className="bg-gray-200 rounded-[12px] w-[1512px] h-[68px] m-auto mt-[20px] mb-[30px] flex items-center justify-around category-slider-container">
+      className="bg-gray-200 rounded-[12px] w-[1512px] h-[68px] m-auto mt-[20px] mb-[30px] flex items-center justify-around category-slider-container"
+      breakpoints={{
+        280: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 0,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      }}>
       {categories.map((categories, i) => (
         <SwiperSlide key={i} className="swiper_item">
           <CategoryPill
